a407901d5696419f9008eacda01ce24d8c427322
TB <bogusz.tomasz@gmail.com>
2021-06-22 11:16:10 +0200

    checking db connection

--- public_html/index.php
+++ public_html/index.php
@@ -1,3 +1,14 @@
 <?php
 
+$host = 'db';
+$user = 'user';
+$pass = 'userpassword';
+$conn = new mysqli($host, $user, $pass);
+
+if ($conn->connect_error) {
+ echo('Error: ' . $conn->connect_error);
+} else {
+ echo 'DB Connected';
+}
+
 phpinfo();
